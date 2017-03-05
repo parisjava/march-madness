530d8cdabad55b8d87eed17482b1dbcacb8384d9
japem <japemstein@Jonahs-MacBook-Pro.local>
2017-03-05 18:53:26 -0500

    Initial R code

---
+++ code.R
@@ -0,0 +1,19 @@
+#Set working directory
+#setwd("Documents/School/Spring17/March madness machine learning")
+
+#Load data
+reg = read.csv("RegularSeasonDetailedResults.csv")
+sample = read.csv("sample_submission.csv")
+seasons = read.csv("Seasons.csv")
+teams = read.csv("Teams.csv")
+tourneyresults = read.csv("TourneyDetailedResults.csv")
+seeds = read.csv("TourneySeeds.csv")
+slots = read.csv("TourneySlots.csv")
+
+#Add columns
+seeds$seedNum = as.numeric(substring(seeds$Seed,2,3))
+tourneyresults = tourneyresults %>% 
+  inner_join(seeds, by=c("Season"="Season","Wteam"="Team")) %>% 
+  rename(WSeed = Seed, WseedNum = seedNum) %>% 
+  inner_join(seeds, by=c("Season"="Season","Lteam"="Team")) %>% 
+  rename(LSeed = Seed, LseedNum = seedNum)
